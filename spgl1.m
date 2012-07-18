322d63fa6b9cbf00ab5ceac275643415c396c02b
saravkin <[sasha.aravkin@gmail.com]>
2012-07-18 14:24:07 -0700

    put in a comment indicating sampling branch

--- spgl1.m
+++ spgl1.m
@@ -1,4 +1,6 @@
 function [x,r,g,info] = spgl1(A, b, tau, sigma, x, options, params)
+% SPGL1 sampling branch
+
 
 %SPGL1  Solve regularized composite programs, including 
 % a) basis pursuit, basis pursuit denoise and lasso
