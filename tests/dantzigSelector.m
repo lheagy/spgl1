4080cb08caf28800b03c81ae5403a637a6d0f37e
Aleksandr Aravkin <sasha.aravkin@gmail.com>
2013-06-11 12:54:42 -0400

    Added new test

---
+++ tests/dantzigSelector.m
@@ -0,0 +1,82 @@
+function [ok] = dantzigSelector()
+
+m = 10; n = 50; k = 2; % m rows, n cols, k nonzeros.
+p = randperm(n); x0 = zeros(n,1); x0(p(1:k)) = sign(randn(k,1));
+A  = randn(m,n); [Q,R] = qr(A',0);  A = Q';
+b  = A*x0 + 0.005 * randn(m,1);
+
+bTild = A'*b;  % b for dantzig selector
+ATild = A'*A;  % A for dantzig selector
+
+p = 30;
+params.p = p;   % p for genNorm
+
+funOne = @funLS;
+funTwo = @genNorm;
+
+
+opts = spgSetParms('optTol',1e-5, ...
+                   'project', @NormL1_project, ...
+                   'primal_norm', @NormL1_primal, ...
+                   'dual_norm', @NormL1_dual, ...
+                   'subspaceMin', 0, ...
+                   'funPenalty', funOne ... 
+                   );
+
+
+% params.nu = 1e-2;
+% params.hub = 0.005;
+% params.rho = 1;
+
+%% Run three separate root finding problems
+
+sigma = .1;
+%sigma = funOne(Err, params);
+
+
+% first run: dantzig with 2-norm, using funLS
+params.funPenalty = funOne;
+[xLS, r, g, info] = spgl1(A, b, [], sigma, [], opts, params);
+
+
+
+% second run: dantzig with 2-norm, using genNorm
+opts = spgSetParms('optTol',1e-5, ...
+                   'project', @NormL1_project, ...
+                   'primal_norm', @NormL1_primal, ...
+                   'dual_norm', @NormL1_dual, ...
+                   'subspaceMin', 0, ...
+                   'funPenalty', funTwo ... 
+                   );
+
+sigma = .1;
+[xGN, r, g, info] = spgl1(ATild, bTild, [], sigma, [], opts, params);
+
+
+ cvx_begin
+   variables xCVX(n)
+   minimize( norm(xCVX,1))  
+   subject to 
+    norms(ATild*xCVX - bTild, p) <= sigma
+ cvx_end
+
+
+fprintf('one norm of standard: %5.3f, one-norm of GN: %5.3f\n', norm(xLS, 1), norm(xGN, 1));
+
+fprintf('norm diff between CVX and Dantz: %5.3f, between CVX and Stand: %5.3f \n', norm(xGN - xCVX, inf),norm(xLS - xCVX, inf));
+
+
+%% Plot the results
+
+figure(1)
+plot(1:n, x0 + 2, 1:n, xLS(1:n), 1:n, xGN(1:n) - 2, 1:n, xCVX(1:n) - 4,   '-k', 'Linewidth', 1.5);
+
+legend('true', 'Standard', 'Dantzig', 'CVX');
+
+figure(2);
+plot(1:m, b - A*x0 + 1.5, 1:m, b - A*xLS(1:n), 1:m, b - A*xGN(1:n) - 1.5, 1:m, b - A*xCVX(1:n) - 3,   '-k', 'Linewidth', 2); 
+legend('true', 'Standard', 'Dantzig', 'CVX');
+
+
+end
+
