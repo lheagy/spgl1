bcd410faaf5939c4d1a7dd5a549b096992a27451
Aleksandr Aravkin <sasha.aravkin@gmail.com>
2013-06-11 15:39:24 -0400

    yeah.

--- tests/dSel.m
+++ tests/dSel.m
@@ -1,6 +1,6 @@
 function [ok] = dantzigSelector()
 
-m = 10; n = 50; k = 2; % m rows, n cols, k nonzeros.
+m = 25; n = 50; k = 5; % m rows, n cols, k nonzeros.
 p = randperm(n); x0 = zeros(n,1); x0(p(1:k)) = sign(randn(k,1));
 A  = randn(m,n); [Q,R] = qr(A',0);  A = Q';
 b  = A*x0 + 0.005 * randn(m,1);
@@ -8,20 +8,20 @@ b  = A*x0 + 0.005 * randn(m,1);
 bTild = A'*b;  % b for dantzig selector
 ATild = A'*A;  % A for dantzig selector
 
-p = 30;
+p = 50;
 params.p = p;   % p for genNorm
 
 funOne = @funLS;
 funTwo = @genNorm;
 
 
-opts = spgSetParms('optTol',1e-5, ...
-                   'project', @NormL1_project, ...
-                   'primal_norm', @NormL1_primal, ...
-                   'dual_norm', @NormL1_dual, ...
-                   'subspaceMin', 0, ...
-                   'funPenalty', funOne ... 
-                   );
+% opts = spgSetParms('optTol',1e-5, ...
+%                    'project', @NormL1_project, ...
+%                    'primal_norm', @NormL1_primal, ...
+%                    'dual_norm', @NormL1_dual, ...
+%                    'subspaceMin', 0, ...
+%                    'funPenalty', funOne ... 
+%                    );
 
 
 % params.nu = 1e-2;
@@ -30,13 +30,12 @@ opts = spgSetParms('optTol',1e-5, ...
 
 %% Run three separate root finding problems
 
-sigma = .1;
 %sigma = funOne(Err, params);
 
 
 % first run: dantzig with 2-norm, using funLS
-params.funPenalty = funOne;
-[xLS, r, g, info] = spgl1(A, b, [], sigma, [], opts, params);
+%params.funPenalty = funOne;
+%[xLS, r, g, info] = spgl1(A, b, [], sigma, [], opts, params);
 
 
 
@@ -49,7 +48,7 @@ opts = spgSetParms('optTol',1e-5, ...
                    'funPenalty', funTwo ... 
                    );
 
-sigma = .1;
+sigma = .05;
 [xGN, r, g, info] = spgl1(ATild, bTild, [], sigma, [], opts, params);
 
 
@@ -61,21 +60,33 @@ sigma = .1;
  cvx_end
 
 
-fprintf('one norm of standard: %5.3f, one-norm of GN: %5.3f\n', norm(xLS, 1), norm(xGN, 1));
+ cvx_begin
+   variables xDantzig(n)
+   minimize( norm(xDantzig,1))  
+   subject to 
+    norms(ATild*xDantzig - bTild, inf) <= sigma
+ cvx_end
 
-fprintf('norm diff between CVX and Dantz: %5.3f, between CVX and Stand: %5.3f \n', norm(xGN - xCVX, inf),norm(xLS - xCVX, inf));
+ 
+ 
+fprintf('one norm of SPGL1: %5.3f, one-norm of P-Dantzig: %5.3f\n', norm(xGN, 1), norm(xCVX, 1));
+
+fprintf('norm diff between CVX and SPGL1: %5.3f \n', norm(xGN - xCVX, inf));
+
+fprintf('norm diff between P-Dantzig and Dantzig: %5.3f\n', norm(xCVX - xDantzig, inf));
+
 
 
 %% Plot the results
 
 figure(1)
-plot(1:n, x0 + 2, 1:n, xLS(1:n), 1:n, xGN(1:n) - 2, 1:n, xCVX(1:n) - 4,   '-k', 'Linewidth', 1.5);
+plot(1:n, x0 + 2,  1:n, xGN(1:n) - 2, 1:n, xCVX(1:n), 1:n, xDantzig(1:n) - 4,   '-k', 'Linewidth', 1.5);
 
-legend('true', 'Standard', 'Dantzig', 'CVX');
+legend('true', 'P-Dantz', 'CVX-P-Dantzig', 'Dantzig');
 
 figure(2);
-plot(1:m, b - A*x0 + 1.5, 1:m, b - A*xLS(1:n), 1:m, b - A*xGN(1:n) - 1.5, 1:m, b - A*xCVX(1:n) - 3,   '-k', 'Linewidth', 2); 
-legend('true', 'Standard', 'Dantzig', 'CVX');
+plot(1:m, b - A*x0 + 1.5, 1:m, b - A*xGN(1:n) - 1.5, 1:m, b - A*xCVX(1:n), 1:m, b - A*xDantzig(1:n) - 3,   '-k', 'Linewidth', 2); 
+legend('true', 'P-Dantz', 'CVX-P-Dantzig', 'Dantzig');
 
 
 end
